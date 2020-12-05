7b3a38f91aa190ba6567ae297017e9124e312525
Nakul Anand <46605231+nakul625@users.noreply.github.com>
2020-12-05 22:56:02 +0530

    Added session change line

--- blog/views.py
+++ blog/views.py
@@ -5,9 +5,11 @@ from django.http import HttpResponse
 
 def index(request):
     myposts = Blogpost.objects.all()
-    print(myposts)
-    return render(request, 'blog/index.html',
-                  {'myposts': myposts})
+    if myposts not in request.session:
+        request.session["myposts"]=[]
+    else:
+        return render(request, 'blog/index.html',
+                  {'myposts': request.session["myposts"]})
 
 def blogpost(request, id):
     post = Blogpost.objects.filter(post_id = id)[0]
